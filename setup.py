c9294c7611eccac506a51fc1c1969934067e91e2
Juan Herruzo <juan@herruzo.dev>
2025-03-20 01:28:00 +0200

    updated merge_device_histories output

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pycalista-ista",
-    version="0.2.0",
+    version="0.3.0",
     author="Juan Herruzo",
     author_email="juan@herruzo.dev",
     description="Python library for the ista calista service.",
